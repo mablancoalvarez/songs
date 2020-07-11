b3f0b32b82a69a07282a9bc434e9abb07c075396
mablanco <mblanco@marketgoo.com>
2020-07-11 15:56:11 +0200

    add combinereducers

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers, combineReducers } from 'redux';
+
+const songsReducers = () => {
+    return [
+        {title: 'No Scrubs', duration: '4:05 '},
+        {title: 'Macarena', duration: '2:35'},
+        {title: 'All Star', duration: '3:15'},
+        {title: 'I Want it That Way', duration: '1:45'},
+    ]
+};
+
+const SelectedSongReducer = (selectedSong= null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+
+}
+
+export default combineReducers ({
+    songs: songsReducers,
+    selectedSong:SelectedSongReducer
+});
