60e75ae06f97abaacaea0096f40379c7e91fede7
mablanco <mblanco@marketgoo.com>
2020-07-13 11:08:46 +0200

    finished second component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import SongList from './SongList';
+import SongDetail from './SongsDetail';
 const App = () => {
-    return (
-      <div>
-         <SongList />
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
       </div>
-    )
+    </div >
+  )
 }
 
 export default App;
